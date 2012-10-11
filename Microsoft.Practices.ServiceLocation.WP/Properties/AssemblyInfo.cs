f611901ebefee820e3505c3b819c8229046baca1
moljac <mcvjetko@holisticware.net>
2012-10-11 14:12:26 +0200

    Initial commit MT and MA project added

---
+++ Microsoft.Practices.ServiceLocation.WP/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Practices.ServiceLocation.WP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Practices.ServiceLocation.WP")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58f9b3a1-8ec8-4b49-95a0-6e471491c8bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 MonoVersal.Microsoft.Practices.ServiceLocation
 ==============================================
 
-Microsoft.Practices.ServiceLocation extended for Mono Mobile platforms (MT and  MA - MonoTouch and Mono for Android)
+Microsoft.Practices.ServiceLocation extended for Mono Mobile platforms 
+(MT and  MA - MonoTouch and Mono for Android)
+
+Port of CommonServiceLocator / CommonServiceLocation
+with extensions for MonoTouch and Mono for Android
+
+License:	http://commonservicelocator.codeplex.com/license
+
+
+http://holisticware.net
+
